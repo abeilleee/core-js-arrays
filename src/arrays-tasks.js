0fcb9304f88217a7740b87ece1eba3cec926d481
May <abeillee@bk.ru>
2024-11-10 19:49:28 +0300

    feat: add oslution for sortDigitNamesByNumericOrder

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -583,8 +583,23 @@ function shiftArray(arr, n) {
  *   sortDigitNamesByNumericOrder([ 'nine','eight','nine','eight' ]) => [ 'eight','eight','nine','nine']
  *   sortDigitNamesByNumericOrder([ 'one','one','one','zero' ]) => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  const numbers = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten',
+  ];
+
+  const result = arr.sort((a, b) => numbers.indexOf(a) - numbers.indexOf(b));
+  return result;
 }
 
 /**
